9eca110afac69a833e751283810be6f6f50030bb
Guilherme Macedo <guilherme@gmacedo.com>
2023-09-25 15:06:13 -0300

    dev
    Signed-off-by: Guilherme Macedo <guilherme@gmacedo.com>

--- scripts/manage_projects.sh
+++ scripts/manage_projects.sh
@@ -3,7 +3,7 @@
 PROJECT_CONFIG_FILE="project_meta.xml"
 
 find . -type f -name "$PROJECT_CONFIG_FILE" | while read f; do
- 	project_name=$(grep -Po '<project name="\K.*?(?=")' $f)
+ 	project_name=$(grep -Po '<project name="\K.*?(?=")' "$f")
 	echo "Creating/Updating project $project_name"
 	osc --config .oscrc meta prj -F "$f" "$project_name"
 	echo ""
